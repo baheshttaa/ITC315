c2b51f077ed7d1e27eb4f67505444ebed600c162
baheshttaa <71807278+baheshttaa@users.noreply.github.com>
2020-09-24 09:16:31 +0430

    added a maximum function

--- discussion.java
+++ discussion.java
@@ -4,6 +4,14 @@ public class practice7 {
     }
     
     public static int min(int x, int y){
+        if (x < y) {
+            return x;
+        }
+        else 
+            return y;
+    }
+    
+    public static int max(int x, int y){
         if (x > y) {
             return x;
         }
