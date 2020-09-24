432d35ff2ee92c04b8767e318df82024e9c772e8
Nshahim <49757836+Nshahim@users.noreply.github.com>
2020-09-24 09:33:53 +0430

    changed absolutely nothing

--- discussion.java
+++ discussion.java
@@ -1,7 +1,7 @@
 public class practice7 {
     public static void main(String[] args) {
         
-        System.out.println("Hello, World");
+        System.out.println("Hello, World")
         
         int a = max(10,8);
         System.out.println(z+" is bigger");
