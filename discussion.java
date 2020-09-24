60f6aaacb4aa690ef1395e5db3880e73cf7bbe8f
baheshttaa <71807278+baheshttaa@users.noreply.github.com>
2020-09-24 09:20:10 +0430

    Update discussion.java

--- discussion.java
+++ discussion.java
@@ -2,6 +2,9 @@ public class practice7 {
     public static void main(String[] args) {
         int a = max(10,8);
         System.out.println(z+" is bigger");
+        
+        int a = min(10,8);
+        System.out.println(z+" is smaller");
     }
     
     public static int min(int x, int y){
